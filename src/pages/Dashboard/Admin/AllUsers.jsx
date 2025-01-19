f115fee05f0cd7da521b3c1561b6204d47a2a694
mahfuzHasan2003 <mahfuzhasan2003@yahoo.com>
2025-01-19 16:02:01 +0600

    added the full functionality of all user page in admin dashboard

--- src/pages/Dashboard/Admin/AllUsers.jsx
+++ src/pages/Dashboard/Admin/AllUsers.jsx
@@ -20,31 +20,86 @@ import { UserPlus, UserMinus } from "lucide-react";
 import { useToast } from "@/hooks/use-toast";
 import UserAvatar from "@/shared/UserAvatar";
 import { useFetchForGet } from "@/hooks/useFetchForGet";
+import {
+   Dialog,
+   DialogContent,
+   DialogDescription,
+   DialogFooter,
+   DialogHeader,
+   DialogTitle,
+} from "@/components/ui/dialog";
+import useAxiosPublic from "@/hooks/useAxiosPublic";
+import useAuth from "@/hooks/useAuth";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const AllUsers = () => {
    const [searchQuery, setSearchQuery] = useState("");
    const [roleFilter, setRoleFilter] = useState("all");
    const { toast } = useToast();
+   const [dialogOpen, setDialogOpen] = useState(false);
+   const [actionUser, setActionUser] = useState(null);
+   const [currentAction, setCurrentAction] = useState(null);
+   const axiosPublic = useAxiosPublic();
+   const { user: loggedInAdmin } = useAuth();
 
-   console.log(searchQuery, roleFilter);
-
-   const { isLoading, data: users = [] } = useFetchForGet(
-      ["allUsers"],
-      `/get-all-users`
+   const TableSkeleton = () => (
+      <>
+         {[...Array(5)].map((_, index) => (
+            <TableRow key={index}>
+               <TableCell>
+                  <Skeleton className='h-4 w-6' />
+               </TableCell>
+               <TableCell>
+                  <Skeleton className='h-10 w-10 rounded-full' />
+               </TableCell>
+               <TableCell>
+                  <Skeleton className='h-4 max-w-[250px]' />
+               </TableCell>
+               <TableCell>
+                  <Skeleton className='h-4 max-w-[200px]' />
+               </TableCell>
+               <TableCell>
+                  <Skeleton className='h-4 max-w-[100px]' />
+               </TableCell>
+               <TableCell className='text-right'>
+                  <Skeleton className='h-8 w-[100px] ml-auto' />
+               </TableCell>
+            </TableRow>
+         ))}
+      </>
    );
 
-   const handleUpdateRole = (userId) => {
-      toast({
-         title: "Role Updated",
-         description: "User role has been updated successfully.",
-      });
-   };
+   const {
+      isLoading,
+      data: users = [],
+      refetch,
+   } = useFetchForGet(
+      ["allUsers", searchQuery, roleFilter],
+      `/get-all-users/${loggedInAdmin?.email}?searchQuery=${searchQuery}&roleFilter=${roleFilter}`,
+      { enabled: !!loggedInAdmin?.email }
+   );
 
-   const handleRemoveAdmin = (userId) => {
-      toast({
-         title: "Admin Removed",
-         description: "Admin privileges have been removed successfully.",
-      });
+   const handleConfirm = async (user) => {
+      if (currentAction === "makeAdmin") {
+         // handleUpdateRole(user._id);
+         await axiosPublic.patch(`/update-user-role/${user._id}`, {
+            newRole: "admin",
+         });
+         toast({
+            title: "Role Updated",
+            description: "User role has been updated successfully.",
+         });
+      } else if (currentAction === "removeAdmin") {
+         await axiosPublic.patch(`/update-user-role/${user._id}`, {
+            newRole: "student",
+         });
+         toast({
+            title: "Admin Removed",
+            description: "Admin privileges have been removed successfully.",
+         });
+      }
+      refetch();
+      setDialogOpen(false);
    };
 
    return (
@@ -87,44 +142,110 @@ const AllUsers = () => {
                   </TableRow>
                </TableHeader>
                <TableBody>
-                  {users.map((user, index) => (
-                     <TableRow key={user._id}>
-                        <TableCell>{index + 1}</TableCell>
-                        <TableCell>
-                           <UserAvatar
-                              userName={user.userName}
-                              imageURL={user.userPhotoURL}
-                           />
-                        </TableCell>
-                        <TableCell className='font-medium'>
-                           {user.userName}
-                        </TableCell>
-                        <TableCell>{user.userEmail}</TableCell>
-                        <TableCell className='capitalize'>
-                           {user.userRole}
-                        </TableCell>
-                        <TableCell className='text-right'>
-                           {user.userRole === "student" && (
-                              <Button
-                                 variant='warning'
-                                 size='sm'
-                                 onClick={() => handleUpdateRole(user._id)}>
-                                 <UserPlus className='h-4 w-4 mr-2' />
-                                 Make Admin
-                              </Button>
-                           )}
-                           {user.userRole === "admin" && (
-                              <Button
-                                 variant='destructive'
-                                 size='sm'
-                                 onClick={() => handleRemoveAdmin(user._id)}>
-                                 <UserMinus className='h-4 w-4 mr-2' />
-                                 Remove from Admin
-                              </Button>
-                           )}
-                        </TableCell>
-                     </TableRow>
-                  ))}
+                  {!loggedInAdmin?.email || isLoading ? (
+                     <TableSkeleton />
+                  ) : (
+                     users.map((user, index) => (
+                        <TableRow key={user._id}>
+                           <TableCell>{index + 1}</TableCell>
+                           <TableCell>
+                              <UserAvatar
+                                 userName={user.userName}
+                                 imageURL={user.userPhotoURL}
+                              />
+                           </TableCell>
+                           <TableCell className='font-medium'>
+                              {user.userName}
+                           </TableCell>
+                           <TableCell>{user.userEmail}</TableCell>
+                           <TableCell className='capitalize'>
+                              {user.userRole}
+                           </TableCell>
+                           <TableCell className='text-right'>
+                              {user.userRole === "student" && (
+                                 <Button
+                                    variant='warning'
+                                    size='sm'
+                                    onClick={() => {
+                                       setCurrentAction("makeAdmin");
+                                       setActionUser(user);
+                                       setDialogOpen(true);
+                                    }}>
+                                    <UserPlus className='h-4 w-4 mr-2' />
+                                    Make Admin
+                                 </Button>
+                              )}
+                              {user.userRole === "admin" && (
+                                 <Button
+                                    variant='destructive'
+                                    size='sm'
+                                    onClick={() => {
+                                       setCurrentAction("removeAdmin");
+                                       setActionUser(user);
+                                       setDialogOpen(true);
+                                    }}>
+                                    <UserMinus className='h-4 w-4 mr-2' />
+                                    Remove from Admin
+                                 </Button>
+                              )}
+
+                              <Dialog
+                                 open={dialogOpen}
+                                 onOpenChange={setDialogOpen}>
+                                 <DialogContent>
+                                    <DialogHeader>
+                                       <DialogTitle>
+                                          {currentAction === "makeAdmin"
+                                             ? "Make Admin"
+                                             : "Remove Admin"}
+                                       </DialogTitle>
+                                    </DialogHeader>
+                                    <DialogDescription>
+                                       Are you sure you want to{" "}
+                                       {currentAction === "makeAdmin" ? (
+                                          <>
+                                             make
+                                             <span className='font-bold'>
+                                                {" "}
+                                                {actionUser?.userName}{" "}
+                                             </span>
+                                             an Admin?
+                                          </>
+                                       ) : (
+                                          <>
+                                             remove
+                                             <span className='font-bold'>
+                                                {" "}
+                                                {actionUser?.userName}{" "}
+                                             </span>
+                                             from Admin?
+                                          </>
+                                       )}
+                                    </DialogDescription>
+                                    <DialogFooter>
+                                       <Button
+                                          variant='outline'
+                                          onClick={() => setDialogOpen(false)}>
+                                          Cancel
+                                       </Button>
+                                       <Button
+                                          variant={
+                                             currentAction === "makeAdmin"
+                                                ? "warning"
+                                                : "destructive"
+                                          }
+                                          onClick={() =>
+                                             handleConfirm(actionUser)
+                                          }>
+                                          Confirm
+                                       </Button>
+                                    </DialogFooter>
+                                 </DialogContent>
+                              </Dialog>
+                           </TableCell>
+                        </TableRow>
+                     ))
+                  )}
                </TableBody>
             </Table>
          </div>
