27eb144221c13101ee746493b01e75cd1c14e9e0
mahfuzHasan2003 <mahfuzhasan2003@yahoo.com>
2025-05-25 16:22:47 +0600

    fixed some issue

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,15 +1,17 @@
 import Footer from "@/components/my-components/Footer";
 import NavBar from "@/components/my-components/NavBar";
 import { Toaster } from "@/components/ui/toaster";
-import { useEffect } from "react";
+import { useEffect} from "react";
 import { Helmet } from "react-helmet";
 import { Outlet, useLocation } from "react-router-dom";
 
 const MainLayout = () => {
+// navigate user to top of the page when change page
   const { pathname } = useLocation();
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
+
   return (
     <div className="min-h-svh flex flex-col">
       <Helmet>
